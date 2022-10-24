f68ba0b4d2c1c2196cd65a64769e2a0cf6ea0173
johannes <johannes.ronning@tietoevry.com>
2022-10-24 17:36:06 +0200

    made abstractions and more classes

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,26 @@
 #include <SDL2/SDL.h>
 #include <stdio.h>
 
-
 #include "app.hpp"
 #include "object.hpp"
-
+#include "sprite.hpp"
+#include "rect.hpp"
 
 int main (int argc, char** argv)
 {
 	App* app = new App();
 	app->initSDL();
-	Object* object = new Object(200);
-	// object->x = 20; already in constructor
-	// specs are optional, will default to 0
-	object->y = 20;
-	object->w = 20;
-	object->h = 20;
-	object->set_color(255, 0, 0, 0);
+	Rect* rect = new Rect(200);
+	rect->y = 20;
+	rect->w = 20;
+	rect->h = 20;
+	rect->set_color(255, 0, 0, 0);
 
 	while (1)
 	{
 		app->prepareScene();
 		app->input();
-		object->render(app->renderer);
+		rect->render(app->renderer);
 		app->presentScene();
 
 
--- src/object.cpp
+++ src/object.cpp
@@ -1,13 +1,7 @@
 #include "object.hpp"
 
-void Object::render(SDL_Renderer* renderer)
-{
-	SDL_Rect rect = this->as_rect();
-	SDL_SetRenderDrawColor(renderer, this->color.r, this->color.g, this->color.b, this->color.a);
-	SDL_RenderFillRect(renderer, &rect);
-}
 
-SDL_Rect Object::as_rect()
+SDL_Rect Object::as_SDL_rect()
 {
 	SDL_Rect rect;
 	rect.x = this->x;
@@ -18,13 +12,6 @@ SDL_Rect Object::as_rect()
 	return rect;
 }
 
-void Object::set_color(int r, int g, int b, int a)
-{
-	this->color.r = r;
-	this->color.g = g;
-	this->color.b = b;
-	this->color.a = a;
-}
 
 Object::Object(int x, int y, int w, int h)
 {
