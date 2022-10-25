4cd94e1abb0ee539f6c1314d55ae991b66a46e55
johannes <johannes.ronning@tietoevry.com>
2022-10-25 19:50:44 +0200

    added particles and particle handler

---
+++ src/particle.cpp
@@ -0,0 +1,22 @@
+#include "particle.hpp"
+#include <iostream>
+
+Particle_Rect::Particle_Rect(int x, int y, int w, int h)
+{
+	this->x = x;
+	this->y = y;
+	this->w = w;
+	this->h = h;
+}
+
+float Particle::reduce_time_to_live(float reduction)
+{
+	// todo: free particle upon death
+	return this->lifetime -= reduction;
+}
+
+void Particle::set_on_death_callback(void*())
+{
+	std::cout << "not yet implemented" << std::endl;
+}
+
